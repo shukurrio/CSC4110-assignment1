0755e6971dedd5ed62641f2db8a938cb8eb5f50b
Shukurillo Ergashov <hw0345@wayne.edu>
2026-01-29 02:35:29 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,16 @@
 // Simple C++ program to display "Hello World"
+//Author: Shukurillo Ergashov <hw0345@wayne.edu>
+
 // Header file for input output functions
 #include<iostream>
+
 using namespace std;
+
 // main function -
 // where the execution of program begins
 int main()
 {
-// prints hello world
-cout<<"Hello World!";
-return 0;
+	// prints hello world
+	cout<<"Hello World!";
+	return 0;
 }
